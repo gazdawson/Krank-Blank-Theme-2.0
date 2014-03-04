e226ac23904675c9afd91e4ca528970f5fa64154
gazdawson <gazdawson@me.com>
2014-03-04 16:10:05 +0000

    added home and contact templates

---
+++ page-contact.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Template Name: Contact
+ * @package Krank Theme
+ */
+?>
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'page'); ?>
